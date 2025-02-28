e995dc382a0ccd3fae5ccaaed8767eadbad6316a
Samuel Crain <samuelbcrain@gmail.com>
2025-02-28 03:49:10 +0000

    Coded method to get sum of two largest ints Integers in List.

--- src/main/java/LargestSum.java
+++ src/main/java/LargestSum.java
@@ -10,6 +10,34 @@ public class LargestSum {
      * @return the largest possible sum of separate numbers from nums.
      */
     public int bigSum(List<Integer> nums){
-        return 0;
+        // if impossible to add, return min value
+        if (nums.size() < 2) {
+            return Integer.MIN_VALUE;
+        }
+        // set up vars
+        int a = nums.get(0);
+        int b = nums.get(1);
+        int temp = 0;
+        // put largest of first two in a, second largest in b
+        if (a < b) {
+            temp = a;
+            a = b;
+            b = temp;
+        }
+
+        // go through list for largest two numbers
+        for (int i = 2; i < nums.size(); i++) {
+            // if find a number larger than current second largest, replace
+            if (nums.get(i) > b) {
+                b = nums.get(i);
+                // when replace, also make sure largest in a and second largest in b
+                if (a < b) {
+                    temp = a;
+                    a = b;
+                    b = temp;
+                }
+            }
+        }
+        return a + b;// return sum of largest numbers
     }
 }
